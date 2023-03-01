be1662493bbcaeef31c365f8396434a851a037dd
ELFrancisco4 <offiongfrancis14@gmail.com>
2023-03-01 14:28:38 +0100

    Change SCSS layout, svg files names and create navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Nav from "./components/Nav/molecules/Nav";
+import "./scss/Globals.scss";
+
 const App = () => {
   return (
     <div className="App">
-
+      <Nav />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
